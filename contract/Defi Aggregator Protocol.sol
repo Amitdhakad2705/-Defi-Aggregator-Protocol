411948762980a39bbec09e24a2b13c5e2c67201c
Amitdhakad2705 <amitdhakad206@gmail.com>
2025-04-26 11:26:57 +0530

    Update Defi Aggregator Protocol.sol

--- contract/Defi Aggregator Protocol.sol
+++ contract/Defi Aggregator Protocol.sol
@@ -3,10 +3,23 @@ pragma solidity ^0.8.19;
 
 interface IDEX {
     function swap(address tokenIn, address tokenOut, uint amountIn) external returns (uint amountOut);
+    // Optional estimate function
+    function getAmountOut(address tokenIn, address tokenOut, uint amountIn) external view returns (uint amountOut);
 }
 
 contract DeFiAggregator {
     address public owner;
+    bool public paused;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not authorized");
+        _;
+    }
+
+    modifier whenNotPaused() {
+        require(!paused, "Contract is paused");
+        _;
+    }
 
     constructor() {
         owner = msg.sender;
@@ -17,23 +30,65 @@ contract DeFiAggregator {
         address tokenIn,
         address tokenOut,
         uint amountIn
-    ) external returns (uint) {
+    ) external whenNotPaused returns (uint) {
         require(amountIn > 0, "Amount must be greater than 0");
 
-        // Approve DEX to spend tokens
+        // Transfer tokens to this contract
         IERC20(tokenIn).transferFrom(msg.sender, address(this), amountIn);
         IERC20(tokenIn).approve(dex, amountIn);
 
-        // Execute swap on DEX
+        // Execute swap
         uint amountOut = IDEX(dex).swap(tokenIn, tokenOut, amountIn);
+
+        // Send output tokens to user
         IERC20(tokenOut).transfer(msg.sender, amountOut);
 
         return amountOut;
     }
 
-    function rescueTokens(address token, uint amount) external {
-        require(msg.sender == owner, "Not authorized");
+    function rescueTokens(address token, uint amount) external onlyOwner {
         IERC20(token).transfer(owner, amount);
+    }
+
+    function updateOwner(address newOwner) external onlyOwner {
+        require(newOwner != address(0), "Invalid address");
+        owner = newOwner;
+    }
+
+    function pause() external onlyOwner {
+        paused = true;
+    }
+
+    function unpause() external onlyOwner {
+        paused = false;
+    }
+
+    function batchSwap(
+        address[] calldata dexes,
+        address[] calldata tokenIns,
+        address[] calldata tokenOuts,
+        uint[] calldata amountsIn
+    ) external whenNotPaused {
+        require(
+            dexes.length == tokenIns.length && 
+            tokenIns.length == tokenOuts.length && 
+            tokenOuts.length == amountsIn.length,
+            "Input array lengths must match"
+        );
+
+        for (uint i = 0; i < dexes.length; i++) {
+            swapTokens(dexes[i], tokenIns[i], tokenOuts[i], amountsIn[i]);
+        }
+    }
+
+    function estimateSwap(
+        address dex,
+        address tokenIn,
+        address tokenOut,
+        uint amountIn
+    ) external view returns (uint) {
+        require(amountIn > 0, "Amount must be greater than 0");
+        return IDEX(dex).getAmountOut(tokenIn, tokenOut, amountIn);
     }
 }
 
@@ -42,4 +97,5 @@ interface IERC20 {
     function transfer(address recipient, uint amount) external returns (bool);
     function approve(address spender, uint amount) external returns (bool);
 }
+
 
